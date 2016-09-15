d0a21e526a553029aa5463f3b9fd955c259fa925
Thomas PIERRAIN <thomas@pierrain.net>
2016-09-15 11:36:10 +0200

    Add first acceptance test

---
+++ RoomsBooking.Tests/RoomBookingEngineTests.cs
@@ -0,0 +1,31 @@
+﻿namespace RoomsBooking.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RoomBookingEngineTests
+    {
+        [Test]
+        public void Should_find_no_room_when_searching_an_empty_location_catalog()
+        {
+            var bookingEngine = new RoomBookingEngine();
+            IEnumerable<Place> availablePlaces = bookingEngine.SearchPlaceToStay(checkInDate:DateTime.Now, checkOutDate:DateTime.Now.AddDays(1), location:"Paris", roomNumber:1, adultsCout:2, childrenCount:0);
+            Assert.AreEqual(0, availablePlaces.Count());
+        }
+    }
+
+    public class RoomBookingEngine
+    {
+        public IEnumerable<Place> SearchPlaceToStay(DateTime checkInDate, DateTime checkOutDate, string location, int roomNumber, int adultsCout, int childrenCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Place
+    {
+    }
+}
