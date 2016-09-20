c5117e27edc9d8359d06def1309dba03ba489271
Thomas PIERRAIN <thomas@pierrain.net>
2016-09-20 23:27:00 +0200

    Start to write the next acceptance test (Write model this time)

---
+++ test/BookARoom.Tests/Acceptance/BookingEngineTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BookARoom.Tests.Acceptance
+{
+    [TestFixture]
+    public class BookingEngineTests
+    {
+        [Test]
+        public void Should_Book_an_available_room()
+        {
+            throw new NotImplementedException("next step ;-)");
+        }
+    }
+}
