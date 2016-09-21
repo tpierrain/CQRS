867ef6f401080c439b8e88c8f8167b8b42197dd3
Thomas PIERRAIN <thomas@pierrain.net>
2016-09-21 14:26:15 +0200

    Make Should_Book_a_room() test GREEN

--- test/BookARoom.Tests/Acceptance/BookingEngineTests.cs
+++ test/BookARoom.Tests/Acceptance/BookingEngineTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace BookARoom.Tests.Acceptance
@@ -7,55 +8,84 @@ namespace BookARoom.Tests.Acceptance
     public class BookingEngineTests
     {
         [Test]
-        public void Should_Book_an_available_room()
+        public void Should_Book_a_room()
         {
-            var bookingStore = new BookingStore();
+            var bookingStore = new BookingStoreStub();
             var bookingHandler = new BookingCommandHandler(bookingStore);
 
             Assert.AreEqual(0, bookingStore.BookingCount);
 
-            var bookingRequest = new BookARoomCommand(placeId: 1, roomNumber: "2", checkinDate: DateTime.Parse("2016-09-17"), checkoutDate: DateTime.Parse("2016-09-18"));
+            var bookingRequest = new BookARoomCommand(clientId: "thomas@pierrain.net", placeId: 1, roomNumber: "2", checkInDate: DateTime.Parse("2016-09-17"), checkOutDate: DateTime.Parse("2016-09-18"));
             bookingHandler.Handle(bookingRequest);
 
             Assert.AreEqual(1, bookingStore.BookingCount);
         }
     }
 
-    public class BookingStore
+    public class BookingStoreStub : IBookingStore
     {
-        public long BookingCount { get; }
+        public long BookingCount { get; private set; }
 
+        private Dictionary<string, List<ICommand>> perClientCommands;
 
+        public BookingStoreStub()
+        {
+            this.perClientCommands = new Dictionary<string, List<ICommand>>();
+        }
+
+        public void BookARoom(BookARoomCommand bookingRequest)
+        {
+            if (!this.perClientCommands.ContainsKey(bookingRequest.ClientId))
+            {
+                this.perClientCommands[bookingRequest.ClientId] = new List<ICommand>();
+            }
+
+            this.perClientCommands[bookingRequest.ClientId].Add(bookingRequest);
+            this.BookingCount++;
+        }
+    }
+
+    public interface IBookingStore
+    {
+        void BookARoom(BookARoomCommand bookingRequest);
     }
 
     public class BookingCommandHandler
     {
-        public BookingStore BookingStore { get; }
+        public BookingStoreStub BookingStore { get; }
 
-        public BookingCommandHandler(BookingStore bookingStore)
+        public BookingCommandHandler(BookingStoreStub bookingStore)
         {
             this.BookingStore = bookingStore;
         }
 
         public void Handle(BookARoomCommand command)
         {
-           
+            this.BookingStore.BookARoom(command);
+
         }
     }
 
-    public class BookARoomCommand
+    public class BookARoomCommand : ICommand
     {
+        public string ClientId { get; }
         public int PlaceId { get; }
         public string RoomNumber { get; }
-        public DateTime CheckinDate { get; }
-        public DateTime CheckoutDate { get; set; }
+        public DateTime CheckInDate { get; }
+        public DateTime CheckOutDate { get; }
 
-        public BookARoomCommand(int placeId, string roomNumber, DateTime checkinDate, DateTime checkoutDate)
+        public BookARoomCommand(string clientId, int placeId, string roomNumber, DateTime checkInDate, DateTime checkOutDate)
         {
+            this.ClientId = clientId;
             this.PlaceId = placeId;
             this.RoomNumber = roomNumber;
-            this.CheckinDate = checkinDate;
-            CheckoutDate = checkoutDate;
+            this.CheckInDate = checkInDate;
+            this.CheckOutDate = checkOutDate;
         }
+    }
+
+    public class ICommand
+    {
+
     }
 }
