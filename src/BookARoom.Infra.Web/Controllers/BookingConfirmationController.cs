79ae214f5f8b1b74a78f2e3ae195acd8c3c04ca4
Thomas PIERRAIN <thomas@pierrain.net>
2016-10-01 19:27:42 +0200

    Add the booking confirmation page and plug it to the Write side of the domain logic

---
+++ src/BookARoom.Infra.Web/Controllers/BookingConfirmationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BookARoom.Domain;
+using BookARoom.Domain.WriteModel;
+using BookARoom.Infra.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookARoom.Infra.Web.Controllers
+{
+    public class BookingConfirmationController : Controller
+    {
+        private readonly ISendCommands bus;
+
+        public BookingConfirmationController(ISendCommands bus)
+        {
+            this.bus = bus;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index(BookingRequestViewModel viewModel)
+        {
+            // Create the task and send it to the bus
+            var bookingCommand = new BookARoomCommand(clientId: viewModel.ClientMail, hotelId: int.Parse(viewModel.HotelId), roomNumber: viewModel.RoomId, checkInDate: viewModel.CheckInDate, checkOutDate: viewModel.CheckOutDate);
+            this.bus.Send(bookingCommand);
+
+            return View(viewModel);
+        }
+    }
+}
